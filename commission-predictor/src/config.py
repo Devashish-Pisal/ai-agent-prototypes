b9ce6ce2cc79edb96254dceda4bd97b3f805f841
Devashish-Pisal <dpisal85@gmail.com>
2026-08-01 19:16:11 +0200

    add paths in config file

---
+++ commission-predictor/src/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+# Folder Paths
+PROJECT_ROOT = Path(__file__).parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+SRC_DIR = PROJECT_ROOT / "src"
+
+# File Paths
+DATASET_FILE_PATH = DATA_DIR / "data.xlsx"
+DATASET_INFO_FILE_PATH = DATA_DIR / "info.xlsx"
+
+
