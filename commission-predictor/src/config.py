3efdc05b5335726ce388237f7db1ee02829ca008
Devashish-Pisal <dpisal85@gmail.com>
2026-08-02 11:34:09 +0200

    add path for processed data

--- commission-predictor/src/config.py
+++ commission-predictor/src/config.py
@@ -9,5 +9,4 @@ SRC_DIR = PROJECT_ROOT / "src"
 # File Paths
 DATASET_FILE_PATH = DATA_DIR / "data.xlsx"
 DATASET_INFO_FILE_PATH = DATA_DIR / "info.xlsx"
-
-
+PROCESSED_DATA = DATA_DIR / "processed_data.xlsx"
